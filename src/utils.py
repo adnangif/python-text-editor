036cfd16d559769b2422d7f9afb3f71bceb2fa8a
ummetalhameem23 <157884536+ummetalhameem23@users.noreply.github.com>
2024-02-04 23:47:13 +0600

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,39 @@ def Open(master: tk.Tk):
     
     return inner_function;
 
+def SaveAs(master: tk.Tk):
+    def inner_function():
+        pass;
+    
+    return inner_function;
 
+def Save(master: tk.Tk):
+    def inner_function():
+        pass;
+    
+    return inner_function;
+
+def Exit(master: tk.Tk):
+    def inner_function():
+        pass;
+    
+    return inner_function;
+
+def CopyAll(master: tk.Tk):
+    def inner_function():
+        pass;
+    
+    return inner_function;
+def Paste(master: tk.Tk):
+    def inner_function():
+        pass;
+    
+    return inner_function;
+
+def Clear(master: tk.Tk):
+    def inner_function():
+        pass;
+    
+    return inner_function;
 if __name__ == "__main__":
     pass
