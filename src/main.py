eaa25ba76ac7a243b0db663869d31b2bb835f0a7
Fahim Adnan <59190871+adnangif@users.noreply.github.com>
2024-02-04 23:38:06 +0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 
 import tkinter as tk
-from utils import *
+import utils
 
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -18,7 +18,7 @@ class App(tk.Tk):
         self.menu_item_File = tk.Menu(self.menu_bar,tearoff=0)
         self.menu_item_Edit = tk.Menu(self.menu_bar,tearoff=0)
 
-        self.menu_item_File.add_command(label="Open",command=fileOpen(master=self))
+        self.menu_item_File.add_command(label="Open",command=utils.Open(master=self))
         self.menu_item_File.add_command(label="Save As")
         self.menu_item_File.add_command(label="Save")
         self.menu_item_File.add_command(label="Exit")
