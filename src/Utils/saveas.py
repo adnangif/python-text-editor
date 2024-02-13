55c6089c13ecb538584ba8ecc9acf2c22f661fda
ummetalhameem23 <157884536+ummetalhameem23@users.noreply.github.com>
2024-02-13 11:33:10 +0600

    added save as feature

--- src/Utils/saveas.py
+++ src/Utils/saveas.py
@@ -6,6 +6,16 @@ import GUI
 
 def SaveAs(master: GUI.App):
     def inner_function(event=None):
-        pass;
-    
-    return inner_function;
+        content=master.main_text_box.get('1.0',tk.END)
+        print(content)
+        master.file_path=filedialog.asksaveasfilename(filetypes=[("txt files ",".txt")],defaultextension=".txt")
+        print(master.file_path)   
+
+        with open(master.file_path,'w') as fw:
+            fw.write(content)
+
+
+
+
+
+    return inner_function
