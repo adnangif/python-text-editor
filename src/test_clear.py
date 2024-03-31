0ce253fc49b6d563835afedbf055630d7b86a687
ummetalhameem23 <157884536+ummetalhameem23@users.noreply.github.com>
2024-04-01 00:39:11 +0600

    test clear function added

---
+++ src/test_clear.py
@@ -0,0 +1,27 @@
+import pytest
+from unittest.mock import MagicMock, patch
+from tkinter import messagebox
+import tkinter as tk
+
+from countwords.countwords import GetCount
+
+
+main = __import__("main")
+utils= __import__("utils")
+
+
+
+
+
+def test_clear_empty_content():
+    app = main.App()
+    utils.Clear(app)()
+    content = app.main_text_box.get('1.0',tk.END)
+    assert content == '\n'   
+
+def test_clear_large_text():
+    app = main.App()
+    app.main_text_box.insert("1.0", "i am\n a\n student and adsf;lkajsdfkcxvzcnv sdfxzcvdfgad dsfasdzx")
+    utils.Clear(app)()
+    content = app.main_text_box.get('1.0',tk.END)
+    assert content == '\n'
